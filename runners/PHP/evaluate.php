d416423139928db7d758dc30ffd36da88c392c5b
FelisPhasma <felisphasma@gmail.com>
2015-12-02 17:11:16 -0700

    Minor code changes and info
    Changes consist of Licensing, regex modifier info, and code comments

--- runners/PHP/evaluate.php
+++ runners/PHP/evaluate.php
@@ -2,22 +2,27 @@
     /**
      * PHP regex runner for Refiddle 
      * Written by FelisPhasma
-     * Copyright (c) FelisPhasma 2015
+     * Copyright (c) FelisPhasma 2015 Under the MIT Licence
      * 
      */
+    
+    // Custom error handeling is used, so turn off the error reporting
     error_reporting(0);
     @ini_set('display_errors', 0);
     
     if(empty($_POST)){
+        // Safeguard against empty requests
         echo '{
           "matchSummary": { 
             "total":0
           }
         }';
     } else {
+        // Get reguests values
         $pattern = $_POST["pattern"];
         $corpus_text = $_POST["corpus_text"];
-
+        
+        // Provide error information for the user
         set_error_handler(function($errno, $errstr, $errfile, $errline) { 
             $json = "{";
                 $json .= '"error":"' . substr($errstr, 18) . '"';
@@ -25,6 +30,8 @@
             echo $json;
             die();
         });
+        
+        // Evaluation
         preg_match_all($pattern, $corpus_text, $matches, PREG_OFFSET_CAPTURE);
         $json = "{";
         $realMatches = $matches[0];
--- runners/PHP/replace.php
+++ runners/PHP/replace.php
@@ -2,21 +2,26 @@
     /**
      * PHP regex runner for Refiddle 
      * Written by FelisPhasma
-     * Copyright (c) FelisPhasma 2015
+     * Copyright (c) FelisPhasma 2015 Under the MIT Licence
      * 
      */
+    
+    // Custom error handeling is used, so turn off the error reporting
     error_reporting(0);
     @ini_set('display_errors', 0);
     
     if(empty($_POST)){
+        // Safeguard against empty requests
         echo '{
           "replace": ""
         }';
     } else {
+        // Get reguests values
         $pattern = $_POST["pattern"];
         $corpus_text = $_POST["corpus_text"];
         $replace_text = $_POST["replace_text"];
         
+        // Provide error information for the user
         set_error_handler(function($errno, $errstr, $errfile, $errline) { 
             $json = "{";
                 $json .= '"error":"' . $errstr . '"';
@@ -24,9 +29,11 @@
             echo $json;
             die();
         });
+        
+        // Corpus text replacements
         $replaced = preg_replace($pattern, $replace_text, $corpus_text);
         $json = "{";
-        $json .= '"replace":' . $replaced;
+            $json .= '"replace":' . $replaced;
         $json .= "}";
         echo $json;
     }
